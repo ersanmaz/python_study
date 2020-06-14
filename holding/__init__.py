5695ef2e71121d2068167ade70b73dd254097c18
Ersan Yilmaz <ersanmaz@gmail.com>
2020-06-14 23:01:29 +0200

    Encapsulation with descriptors.

--- holding/__init__.py
+++ holding/__init__.py
@@ -1,1 +1,3 @@
 from holding.holding import *
+from holding.validate import *
+from holding.validated_holding import *
---
+++ holding/validated_holding.py
@@ -0,0 +1,23 @@
+from holding.validate import String, Integer, Float
+
+
+class Holding(object):
+    name = String('name')
+    shares = Integer('shares')
+    price = Float('float')
+
+    def __init__(self, name, date, shares, price):
+        self.name = name
+        self.date = date
+        self.shares = shares
+        self.price = price
+
+    def __repr__(self):
+        return 'Holding({!r}, {!r}, {!r}, {!r})'.format(self.name, self.date, self.shares, self.price)
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, shares):
+        self.shares -= shares
