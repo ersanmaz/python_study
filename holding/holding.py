8b44800dca6f0115f0ad8ec03d6f67eee71328ef
Ersan Yilmaz <ersanmaz@gmail.com>
2020-06-14 22:21:40 +0200

    Encapsulation.

--- holding/holding.py
+++ holding/holding.py
@@ -5,9 +5,22 @@ class Holding(object):
         self.shares = shares
         self.price = price
 
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, new_price):
+        if not isinstance(new_price, float):
+            raise TypeError('Expected float')
+        if new_price < 0:
+            raise ValueError('Must >=0')
+        self._price = new_price
+
     def __repr__(self):
         return 'Holding({!r}, {!r}, {!r}, {!r})'.format(self.name, self.date, self.shares, self.price)
 
+    @property
     def cost(self):
         return self.shares * self.price
 
